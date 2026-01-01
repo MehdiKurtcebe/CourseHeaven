04314bc013948dd9582af12e63518510cdd54bf5
mehdi.kurtcebe <mehdi.kurtcebe@hotmail.com>
2026-01-02 00:26:15 +0300

    (Catalog) Use IRequestByServiceResult

--- CourseHeaven.Catalog.Api/Features/Categories/GetById/GetCategoryByIdEndpoint.cs
+++ CourseHeaven.Catalog.Api/Features/Categories/GetById/GetCategoryByIdEndpoint.cs
@@ -8,7 +8,7 @@ using MediatR;
 
 namespace CourseHeaven.Catalog.Api.Features.Categories.GetById;
 
-public record GetCategoryByIdQuery(Guid Id) : IRequest<ServiceResult<CategoryDto>>;
+public record GetCategoryByIdQuery(Guid Id) : IRequestByServiceResult<CategoryDto>;
 
 public class GetCategoryByIdHandler(AppDbContext context, IMapper mapper)
     : IRequestHandler<GetCategoryByIdQuery, ServiceResult<CategoryDto>>
