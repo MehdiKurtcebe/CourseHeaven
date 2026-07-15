5796656ad4a9d15f5b790b6eb9d3bf97ec1909e1
Mehdi Kurtcebe <45846466+MehdiKurtcebe@users.noreply.github.com>
2026-07-15 18:46:31 +0300

    (Gateway) add auth

--- CourseHeaven.Gateway/Program.cs
+++ CourseHeaven.Gateway/Program.cs
@@ -1,8 +1,14 @@
+using CourseHeaven.Shared.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddAuthenticationAuthorizationExtension(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapReverseProxy();
 app.MapGet("/", () => "YARP Gateway");
