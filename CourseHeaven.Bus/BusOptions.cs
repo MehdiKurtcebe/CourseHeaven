12fe4631cc537879dc53cd11d7883f8673bc5c31
Mehdi Kurtcebe <45846466+MehdiKurtcebe@users.noreply.github.com>
2026-07-30 17:34:15 +0300

    (Bus) Scaffold Bus service

---
+++ CourseHeaven.Bus/BusOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseHeaven.Bus;
+
+public class BusOptions
+{
+    public required string Address { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public required int Port { get; set; }
+}
---
+++ CourseHeaven.Bus/MassTransitConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CourseHeaven.Bus;
+
+public static class MassTransitConfigurationExtensions
+{
+    public static IServiceCollection AddMassTransitExtension(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var busOptions = (configuration.GetSection(nameof(BusOptions)).Get<BusOptions>())!;
+
+        services.AddMassTransit(configurator =>
+        {
+            configurator.UsingRabbitMq((_, cfg) =>
+            {
+                cfg.Host(new Uri($"rabbitmq://{busOptions.Address}:{busOptions.Port}"), h =>
+                {
+                    h.Username(busOptions.Username);
+                    h.Password(busOptions.Password);
+                });
+            });
+        });
+
+        return services;
+    }
+}
