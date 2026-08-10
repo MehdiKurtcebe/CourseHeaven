626d277fa36b95ae8564ab2783b463171e0bf1da
Mehdi Kurtcebe <45846466+MehdiKurtcebe@users.noreply.github.com>
2026-08-11 02:35:10 +0300

    (Bus) Add commands and events for course image upload functionality

---
+++ CourseHeaven.Bus/Commands/UploadCourseImageCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace CourseHeaven.Bus.Commands;
+
+public record UploadCourseImageCommand(Guid CourseId, byte[] Image, string FileName);
---
+++ CourseHeaven.Bus/Events/CourseImageUploadedEvent.cs
@@ -0,0 +1,3 @@
+﻿namespace CourseHeaven.Bus.Events;
+
+public record CourseImageUploadedEvent(Guid CourseId, string ImageUrl);
