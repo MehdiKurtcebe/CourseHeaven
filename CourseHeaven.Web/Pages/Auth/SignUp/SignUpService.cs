09e7f9b02f4dad6c536c7669c4e799ada5e4561a
Mehdi Kurtcebe <45846466+MehdiKurtcebe@users.noreply.github.com>
2026-08-17 01:35:58 +0300

    (Web) Fix KeyCloakErrorResponse in Sign up

--- CourseHeaven.Web/Pages/Auth/SignUp/SignUpService.cs
+++ CourseHeaven.Web/Pages/Auth/SignUp/SignUpService.cs
@@ -5,16 +5,15 @@ using Duende.IdentityModel.Client;
 
 namespace CourseHeaven.Web.Pages.Auth.SignUp;
 
-public record KeyCloakErrorResponse(string ErrorMessage);
+public record KeyCloakErrorResponse(string Error);
 
 public class SignUpService(IdentityOptions identityOptions, HttpClient client, ILogger<SignUpService> logger)
 {
-    
     public async Task<ServiceResult> CreateAccountAsync(SignUpViewModel model, CancellationToken cancellationToken)
     {
         var token = await GetClientCredentialTokenAsAdminAsync(cancellationToken);
         client.SetBearerToken(token);
-        
+
         var address = $"{identityOptions.BaseAddress}/admin/realms/{identityOptions.Realm}/users";
         var userCreateRequest = CreateUserCreateRequest(model);
 
@@ -23,19 +22,20 @@ public class SignUpService(IdentityOptions identityOptions, HttpClient client, I
         {
             if (response.StatusCode != HttpStatusCode.InternalServerError)
             {
-                var keyCloakErrorResponse = await response.Content.ReadFromJsonAsync<KeyCloakErrorResponse>(cancellationToken);
-                return ServiceResult.Error(keyCloakErrorResponse!.ErrorMessage);
+                var keyCloakErrorResponse =
+                    await response.Content.ReadFromJsonAsync<KeyCloakErrorResponse>(cancellationToken);
+                return ServiceResult.Error(keyCloakErrorResponse!.Error);
             }
-            
+
             var error = await response.Content.ReadAsStringAsync(cancellationToken);
             logger.LogError(error);
 
             return ServiceResult.Error("System error occurred while creating the account. Please try again later.");
         }
-        
+
         return ServiceResult.Success();
     }
-    
+
     private static UserCreateRequest CreateUserCreateRequest(SignUpViewModel model)
     {
         return new UserCreateRequest(
@@ -46,10 +46,10 @@ public class SignUpService(IdentityOptions identityOptions, HttpClient client, I
             model.Email,
             [new Credential("password", model.Password, false)]);
     }
-    
+
     private async Task<string> GetClientCredentialTokenAsAdminAsync(CancellationToken cancellationToken)
     {
-        var discoveryRequest = new DiscoveryDocumentRequest()
+        var discoveryRequest = new DiscoveryDocumentRequest
         {
             Address = identityOptions.Address,
             Policy = { RequireHttps = false }
@@ -58,22 +58,17 @@ public class SignUpService(IdentityOptions identityOptions, HttpClient client, I
         client.BaseAddress = new Uri(identityOptions.Address);
         var discoveryResponse = await client.GetDiscoveryDocumentAsync(discoveryRequest, cancellationToken);
         if (discoveryResponse.IsError)
-        {
             throw new Exception($"Discovery document request failed: {discoveryResponse.Error}");
-        }
 
         var tokenResponse = await client.RequestClientCredentialsTokenAsync(
             new ClientCredentialsTokenRequest
             {
                 Address = discoveryResponse.TokenEndpoint,
-                ClientId = identityOptions.Admin.ClientId,
-                ClientSecret = identityOptions.Admin.ClientSecret,
+                ClientId = identityOptions.Web.ClientId,
+                ClientSecret = identityOptions.Web.ClientSecret
             },
             cancellationToken);
-        if (tokenResponse.IsError)
-        {
-            throw new Exception($"Token request failed: {tokenResponse.Error}");
-        }
+        if (tokenResponse.IsError) throw new Exception($"Token request failed: {tokenResponse.Error}");
 
         return tokenResponse.AccessToken!;
     }
